58310576709207aa13b900df19a81a54d92c1257
William Knowles-Kellett <39969985+wknowleskellett@users.noreply.github.com>
2024-02-10 16:01:56 -0500

    Downloaded template

---
+++ src/client/java/dev/williamknowleskellett/tntree/TntreeClient.java
@@ -0,0 +1,10 @@
+package dev.williamknowleskellett.tntree;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TntreeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/dev/williamknowleskellett/tntree/TntreeDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.williamknowleskellett.tntree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TntreeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
